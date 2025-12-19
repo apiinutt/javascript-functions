c98f8002c7dfba23d5909c58cdd37d0f15782b54
apiinutt <apinut19501@gmail.com>
2025-12-19 21:24:36 +0700

    submit assignment

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,24 @@
 // Start coding here
-let calculator = {};
+const add = (a,b) => {
+        return a+b;
+}
+const subtract = (a,b) => {
+    return a-b;
+}
+const multiply = (a,b) => {
+    return a*b;
+}
+const divide = (a,b) => {
+    return a/b;
+}
+
+let calculator = {
+    add: add(),
+    subtract: subtract(),
+    multiply: multiply(),
+    divide: divide(),
+};
+
+let addResult = add(10,20);
+let addDivide = divide(3000,10);
+console.log(addResult,addDivide)
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,10 @@
 // Start coding here
-const minNumber = findMinNumber(10, 20);
+// const minNumber = findMinNumber(10, 20);
 
-console.log(minNumber); // 10
+// console.log(minNumber); // 10
+
+const findMinNumber = (a,b) => {
+    const findMinNumber = Math.min(a,b);
+    return findMinNumber
+}
+console.log(findMinNumber(569,158))
